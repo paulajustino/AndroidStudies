050589d3700f5a8a2c4300c90748fbe8ffde486f
Paula Ribeiro <rpaulaeduarda@gmail.com>
2023-02-02 10:14:00 -0300

    some updates to exercise6

--- app/src/main/java/com/paulajustino/androidstudies/Classes.kt
+++ app/src/main/java/com/paulajustino/androidstudies/Classes.kt
@@ -2,7 +2,7 @@ package com.paulajustino.androidstudies
 
 class Classes
 
-class OldVehicle(
+class Vehicle(
     private var velocity: Long = 0,
     private var acceleration: Long = 10,
     var model: String
@@ -21,8 +21,8 @@ class OldVehicle(
 }
 
 fun main() {
-    val vehicle1: OldVehicle = OldVehicle(velocity = 60, model = "fiat mobi")
-    val vehicle2: OldVehicle = OldVehicle(velocity = 80, acceleration = 20, model = "audi A3")
+    val vehicle1 = Vehicle(velocity = 60, model = "fiat mobi")
+    val vehicle2 = Vehicle(velocity = 80, acceleration = 20, model = "audi A3")
 
     println(vehicle1.model)
     println(vehicle1.getActualVelocity())
--- app/src/main/java/com/paulajustino/androidstudies/Polymorphism.kt
+++ app/src/main/java/com/paulajustino/androidstudies/Polymorphism.kt
@@ -2,10 +2,12 @@ package com.paulajustino.androidstudies
 
 class Polymorphism
 
-abstract class Vehicle {
-    private var velocity: Long = 0
+// ------------------- Activity 1 -------------------
+abstract class Vehicle1(
+    open var model: String,
+) {
+    var velocity: Long = 0
     abstract var acceleration: Long
-    abstract var model: String
 
     fun increaseVelocity() {
         this.velocity = this.velocity + this.acceleration
@@ -19,36 +21,129 @@ abstract class Vehicle {
 
     fun getActualVelocity() = this.velocity
 
-    override fun toString() : String {
+    override fun toString(): String {
+        return "model = $model \n" +
+                "velocity = $velocity \n" +
+                "acceleration = $acceleration \n"
+    }
+
+    abstract fun printVehicleVelocity(): String
+}
+
+class Car1(
+    override var model: String
+) : Vehicle1(model) {
+    override var acceleration: Long = 2
+
+    override fun printVehicleVelocity(): String {
+        val velocity = super.getActualVelocity()
+        return "velocity: $velocity of the vehicle: Car"
+    }
+}
+
+class Motorcycle1(
+    override var model: String
+) : Vehicle1(model) {
+    override var acceleration: Long = 5
+
+    override fun printVehicleVelocity(): String {
+        val velocity = super.getActualVelocity()
+        return "velocity: $velocity of the vehicle: Motorcycle"
+    }
+}
+
+// ------------------- Activity 2 -------------------
+interface Vehicle2 {
+    var velocity: Long
+    var acceleration: Long
+    var model: String
+
+    fun increaseVelocity() {
+        this.velocity = this.velocity + this.acceleration
+    }
+
+    fun decreaseVelocity() {
+        this.velocity = this.velocity - this.acceleration
+        if (this.velocity < 0)
+            this.velocity = 0
+    }
+}
+
+class Car2(
+    override var velocity: Long = 0,
+    override var acceleration: Long,
+    override var model: String
+) : Vehicle2 {
+    override fun toString(): String {
         return "model = $model \n" +
                 "velocity = $velocity \n" +
                 "acceleration = $acceleration \n"
     }
 }
 
-class Car(
-    override var acceleration: Long,
-    override var model: String,
-) : Vehicle()
-
-class Motorcycle(
+class Motorcycle2(
+    override var velocity: Long = 0,
     override var acceleration: Long = 5,
-    override var model: String,
-) : Vehicle()
+    override var model: String
+) : Vehicle2 {
+    override fun toString(): String {
+        return "model = $model \n" +
+                "velocity = $velocity \n" +
+                "acceleration = $acceleration \n"
+    }
+}
+
+// ------------------- Activity 3 -------------------
+class VehicleFactory {
+    fun createAnVehicle(type: String, model: String = ""): Vehicle1? {
+        return when (type) {
+            "car" -> Car1(model)
+            "moto" -> Motorcycle1(model)
+            else -> null
+        }
+    }
+}
 
 fun main() {
-    val car: Car = Car(acceleration = 1, model = "audi A3")
-    val moto: Motorcycle = Motorcycle(model = "honda 160")
+    // ------------------- Activity 1 -------------------
+    val car1 = Car1(model = "audi A3")
+    val moto1 = Motorcycle1(model = "honda 160")
 
-    println(car.getActualVelocity())
-    repeat (2) {
-        car.increaseVelocity()
-    }
-    println(car)
+    car1.decreaseVelocity()
+    println(car1)
 
-    println(moto.getActualVelocity())
-    repeat(3) {
-        moto.decreaseVelocity()
+    moto1.increaseVelocity()
+    println(moto1)
+
+    // ------------------- Activity 2 -------------------
+    val car2 = Car2(acceleration = 2, model = "fusion")
+    val moto2 = Motorcycle2(velocity = 10, model = "bmw gs")
+
+    car2.decreaseVelocity()
+    println(car2)
+
+    moto2.increaseVelocity()
+    println(moto2)
+
+    // ------------------- Activity 3 -------------------
+    val factory = VehicleFactory()
+
+    val vehicleList: MutableList<Vehicle1> = mutableListOf()
+    val vehicle1 = factory.createAnVehicle(type = "car", model = "audi A3")
+    vehicle1?.let {
+        vehicleList.add(vehicle1)
     }
-    println(moto)
+    val vehicle2 = factory.createAnVehicle(type = "moto", model = "honda 160")
+    vehicle2?.let {
+        vehicleList.add(vehicle2)
+    }
+    val vehicle3 = factory.createAnVehicle("bike")
+    vehicle3?.let {
+        vehicleList.add(vehicle3)
+    } // vehicle3 must be null
+
+    vehicleList.forEach {
+        it.increaseVelocity()
+        println(it.printVehicleVelocity())
+    }
 }
